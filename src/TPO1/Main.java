16247417363e8225e0a0723287517c93254851a2
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2021-08-29 14:00:36 -0300

    Comentarios

--- src/TPO1/Main.java
+++ src/TPO1/Main.java
@@ -7,14 +7,14 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 	public static void main(String[] args) {
 		GeneradorListaImagenes generador= new GeneradorListaImagenes();
-		List <Imagen>imagenes= generador.generar(300);//Generara 20 imagenes de como maximo 50mb
+		List <Imagen>imagenes= generador.generar(50);//Generara 20 imagenes de como maximo 50mb
 		
-		ProcesadorImagenes procesadorImagen= new ProcesadorImagenes(imagenes,0,imagenes.size());
+		ProcesadorImagenes procesadorImagen= new ProcesadorImagenes(imagenes,0,imagenes.size()); //La pool la manejaria pedco asi que de aca para abajo en pedco
 		ForkJoinPool pool = new ForkJoinPool();
 		pool.execute(procesadorImagen);
 		
-		procesadorImagen.join();//Espero a que termine
-		pool.shutdown();
+		procesadorImagen.join();//Espero a que termine, si no el main termina antes y no llega a mostrar nada por consola
+		
 		if(procesadorImagen.isCompletedNormally()) {
 			System.out.println("TERMINO CON EXITO");
 		}
